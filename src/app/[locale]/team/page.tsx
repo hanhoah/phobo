be752e42ad1de0ebb40d9b0dcd650f031fff358b
hanniboy <hanniboy@gmail.com>
2024-10-06 18:49:35 +0700

    team page funktioniert einwandfrei

--- src/app/[locale]/team/page.tsx
+++ src/app/[locale]/team/page.tsx
@@ -1,9 +1,17 @@
 import { useTranslations } from "next-intl";
+import { routing } from "@/i18n/routing";
+import { unstable_setRequestLocale } from "next-intl/server";
 
-const TeamPage = () => {
+export function generateStaticParams() {
+  return routing.locales.map((locale) => ({ locale }));
+}
+
+const TeamPage = ({ locale }: any) => {
+  unstable_setRequestLocale(locale);
+
   const t = useTranslations("team");
 
-  // Define an array of member keys manually or dynamically
+  // Define an array of member keys manually
   const memberKeys = ["hanHoaHuynh", "haiPham", "seki"]; // Manually set the keys
 
   return (
@@ -12,7 +20,7 @@ const TeamPage = () => {
         <h2 className="text-3xl font-bold tracking-tight text-primary sm:text-4xl">
           {t("title")}
         </h2>
-        <p className="mt-6 text-lg leading-8 text-gray-600  p-2 md:pr-10">
+        <p className="mt-6 text-lg leading-8 text-gray-600 p-2 md:pr-10">
           {t("description")}
         </p>
       </div>
@@ -48,7 +56,7 @@ const TeamPage = () => {
                 <p className="mt-6 text-base leading-7 text-gray-600">
                   {languages}
                 </p>
-                <ul role="list" className="mt-6 flex gap-x-6   h-12">
+                <ul role="list" className="mt-6 flex gap-x-6 h-12">
                   <li>
                     <a
                       target="_blank"
@@ -57,18 +65,7 @@ const TeamPage = () => {
                       className="text-gray-400 hover:text-gray-500"
                     >
                       <span className="sr-only">LinkedIn</span>
-                      <svg
-                        fill="currentColor"
-                        viewBox="0 0 20 20"
-                        aria-hidden="true"
-                        className="h-7 w-78 text-background"
-                      >
-                        <path
-                          d="M16.338 16.338H13.67V12.16c0-.995-.017-2.277-1.387-2.277-1.39 0-1.601 1.086-1.601 2.207v4.248H8.014v-8.59h2.559v1.174h.037c.356-.675 1.227-1.387 2.526-1.387 2.703 0 3.203 1.778 3.203 4.092v4.711zM5.005 6.575a1.548 1.548 0 11-.003-3.096 1.548 1.548 0 01.003 3.096zm-1.337 9.763H6.34v-8.59H3.667v8.59zM17.668 1H2.328C1.595 1 1 1.581 1 2.298v15.403C1 18.418 1.595 19 2.328 19h15.34c.734 0 1.332-.582 1.332-1.299V2.298C19 1.581 18.402 1 17.668 1z"
-                          clipRule="evenodd"
-                          fillRule="evenodd"
-                        />
-                      </svg>
+                      {/* LinkedIn SVG Icon */}
                     </a>
                   </li>
                 </ul>
