0d6c09947bffc77697c1cab3f6523783f1880106
hanniboy <hanniboy@gmail.com>
2025-01-31 14:51:02 +0700

    standard update

--- src/app/[locale]/blog/[slug]/page.tsx
+++ src/app/[locale]/blog/[slug]/page.tsx
@@ -1,41 +1,63 @@
-import { getPostData } from "@/lib/markdown";
-
-interface Post {
-  title: string;
-  slug: string;
-  date: string;
-  description: string;
-}
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+import { Metadata } from "next";
+import { getTranslations } from "next-intl/server";
+import Markdown from "markdown-to-jsx";
 
 interface Params {
-  slug: string;
   locale: string;
+  slug: string;
 }
 
-const BlogPost = ({
-  frontmatter,
-  content,
+export async function generateMetadata({
+  params,
 }: {
-  frontmatter: any;
-  content: string;
-}) => {
+  params: Params;
+}): Promise<Metadata> {
+  const { locale, slug } = params;
+  const postFilePath = path.join(
+    process.cwd(),
+    "src/posts",
+    locale,
+    `${slug}.md`
+  );
+  const fileContent = fs.readFileSync(postFilePath, "utf8");
+  const { data } = matter(fileContent);
+
+  return {
+    title: data.title,
+    description: data.description,
+    keywords: data.keywords,
+  };
+}
+
+const BlogPostPage = async ({ params }: { params: Params }) => {
+  const { locale, slug } = params;
+
+  // Pfad zur Markdown-Datei
+  const postFilePath = path.join(
+    process.cwd(),
+    "src/posts",
+    locale,
+    `${slug}.md`
+  );
+  const fileContent = fs.readFileSync(postFilePath, "utf8");
+
+  // Frontmatter und Inhalt extrahieren
+  const { data, content } = matter(fileContent);
+
   return (
-    <div>
-      <h1>{frontmatter.title}</h1>
-      <div dangerouslySetInnerHTML={{ __html: content }} />
-    </div>
+    <article className="prose max-w-4xl mx-auto p-4">
+      <h1>{data.title}</h1>
+      <div className="text-gray-500 mb-8">
+        <time dateTime={data.date}>
+          {new Date(data.date).toLocaleDateString()}
+        </time>
+      </div>
+      <Markdown>{content}</Markdown>
+    </article>
   );
 };
 
-export default async function Page({ params }: { params: Params }) {
-  const { locale, slug } = params;
-  const { frontmatter, content } = await getPostData(slug, locale);
-
-  return (
-    <div className="flex flex-row">
-      <div className="md:w-3/5  px-2">
-        <BlogPost frontmatter={frontmatter} content={content} />
-      </div>
-    </div>
-  );
-}
+export default BlogPostPage;
